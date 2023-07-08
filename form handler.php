c96ef45ff03bf425bb4b50c57239837a78aa04a7
Arshu Mardi <130129383+arrsu@users.noreply.github.com>
2023-07-08 20:17:26 +0530

    Add files via upload

---
+++ form handler.php
@@ -0,0 +1,29 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'arshumardi123@gmail.com';
+
+$email_subject = 'New form Submission';
+
+$email_body = "User Name: $name.\n",
+              "User Email: $visitor_email.\n",
+              "Subject: $subject.\n",
+              "user Message: $message.\n";
+
+$to = 'arshumardi67@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "reply-To: $visitor_email \r\n";
+
+email($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+
+?>
